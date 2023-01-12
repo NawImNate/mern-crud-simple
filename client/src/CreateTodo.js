c9efb25766f252f8bd30377fbd119dfbaa72a121
nawimnate <jnhatch3@gmail.com>
2023-01-12 17:19:55 -0500

    created text fields and handle change values

--- client/src/CreateTodo.js
+++ client/src/CreateTodo.js
@@ -1,7 +1,49 @@
+import { Button, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 export default function CreateTodo() {
+  const [todo, setTodo] = useState({ title: "", description: "" });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+
+    setTodo((prev) => {
+      return {
+        ...prev,
+        [id]: value,
+      };
+    });
+  };
+  useEffect(() => {
+    console.log(todo);
+  }, [todo]);
+
   return (
-    <div>
-      <h1>Create a todo ...</h1>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <h1>Create Todo</h1>
+      <TextField
+        id="title"
+        className="input"
+        label="Title"
+        variant="outlined"
+        onChange={handleChange}
+      />
+      <TextField
+        id="description"
+        className="input"
+        label="Description"
+        variant="outlined"
+        style={{ marginTop: "1rem" }}
+        onChange={handleChange}
+      />
+      <Button
+        variant="outlined"
+        style={{ marginTop: "1rem" }}
+        onClick={() => navigate(-1)}
+      >
+        Back
+      </Button>
     </div>
   );
 }
