2b7f3e68e7dc7543911a6e7eabad10c3e82707c2
nawimnate <jnhatch3@gmail.com>
2023-01-12 16:16:49 -0500

    implement useNavigate for main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 export default function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
       <h1>Welcome To My Todo App</h1>
+      <Button variant="outlined" onClick={() => navigate("create")}>
+        Next
+      </Button>
     </div>
   );
 }
